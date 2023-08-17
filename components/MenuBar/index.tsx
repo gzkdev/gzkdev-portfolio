278549ec36de12312c95127a637bff7dfa4d203f
Ezihe Godswill <ezihegodswill01@gmail.com>
2023-08-17 15:18:13 +0100

    implement navbar component with animation

--- components/MenuBar/index.tsx
+++ components/MenuBar/index.tsx
@@ -1,6 +1,21 @@
+"use client";
+import {
+  useMotionValue,
+  useTransform,
+  motion,
+  MotionValue,
+  useSpring,
+} from "framer-motion";
 import Link from "next/link";
+import { useRef } from "react";
 
-const navigationMap = [
+type Navigation = {
+  name: string;
+  url: string;
+  icon: null;
+};
+
+const navigationMap: Navigation[] = [
   {
     name: "Home",
     url: "/",
@@ -34,17 +49,51 @@ const navigationMap = [
 ];
 
 export default function MenuBar() {
+  let mouseX = useMotionValue(Infinity);
+
   return (
-    <div className="fixed bottom-8 left-[50%] z-50 max-w-[400px] translate-x-[-50%] rounded-full border border-[#fdfdfd08] bg-[#fdfdfd0f] p-1">
-      <div className="flex items-center justify-center gap-2">
-        {navigationMap.map((item) => (
-          <Link
-            key={item.name}
-            href={item.url}
-            className="flex h-12 w-12 items-baseline justify-center rounded-full border border-[#fdfdfd08] bg-black"
-          ></Link>
-        ))}
-      </div>
+    <div
+      onMouseMove={(e) => mouseX.set(e.pageX)}
+      onMouseLeave={() => mouseX.set(Infinity)}
+      className="fixed bottom-8 left-[50%] z-50 flex max-h-12 max-w-max translate-x-[-50%] items-end justify-center gap-2  rounded-full border border-[#fdfdfd08] bg-[#fdfdfd0f] p-1 py-0.5"
+    >
+      {navigationMap.map((navigation) => (
+        <NavigationLink
+          key={navigation.name}
+          mouseX={mouseX}
+          navigation={navigation}
+        />
+      ))}
     </div>
+  );
+}
+
+function NavigationLink({
+  mouseX,
+  navigation,
+}: {
+  mouseX: MotionValue;
+  navigation: Navigation;
+}) {
+  const ref = useRef<HTMLSpanElement>(null);
+  let distance = useTransform(mouseX, (val) => {
+    let bounds = ref.current?.getBoundingClientRect() ?? { x: 0, width: 0 };
+    return val - bounds.x - bounds?.width / 2;
+  });
+  let computeWidth = useTransform(distance, [-200, 0, 100], [40, 80, 40]);
+  let width = useSpring(computeWidth, {
+    damping: 15,
+    mass: 0.1,
+    stiffness: 200,
+  });
+
+  return (
+    <Link href={navigation.url}>
+      <motion.span
+        ref={ref}
+        style={{ width }}
+        className="flex aspect-square items-baseline justify-center rounded-full border border-[#fdfdfd08] bg-black"
+      ></motion.span>
+    </Link>
   );
 }
