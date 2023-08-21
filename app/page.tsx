62ab302d28c38b80b766dbdad5e0132783d3ce4b
Ezihe Godswill <ezihegodswill01@gmail.com>
2023-08-21 22:47:52 +0100

    latest update

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Loader from "@/components/Loader";
+// import Loader from "@/components/Loader";
 import Image from "next/image";
 import ProfileImage from "../public/assets/images/profile-4.jpg";
 import {
@@ -7,68 +7,41 @@ import {
   IoLogoLinkedin,
   IoLogoCodepen,
 } from "react-icons/io";
+import Menu from "@/components/Menu";
 
 export default function Home() {
   return (
-    <main className="grid min-h-full lg:max-h-screen lg:grid-cols-4 lg:grid-rows-6">
-      {/* [&>*]:outline */}
-      <Loader text="home" />
-      <div className="grid-item top-0 lg:sticky lg:col-[1/3] lg:row-[1/6]">
-        <div className="wrapper h-full w-full  lg:text-start">
-          <div className="sticky-header">Intro</div>
+    <main className="grid min-h-full gap-[--grid-gutter] p-[--grid-gutter] [--grid-gutter:0px] lg:grid-cols-4 lg:grid-rows-4">
+      <div className="grid-item top-[--grid-gutter] lg:sticky lg:col-[1/3] lg:row-[1/5] lg:max-h-[calc(100vh-(2*var(--grid-gutter)))]">
+        <div className="wrapper h-full w-full">
+          <div className="sticky-header">
+            <Menu />
+          </div>
           <div className="flex w-full flex-col gap-5 px-5 py-5 pb-10 lg:items-start lg:pb-5">
-            <div className="relative aspect-square w-full max-w-[144px] overflow-hidden rounded border-[4px] border-teal-500 grayscale-[50%] transition hover:grayscale-[30%]">
+            <div className="relative aspect-square w-full max-w-[144px] cursor-nw-resize overflow-hidden border-2 border-teal-500 grayscale-[50%] transition hover:border-transparent hover:grayscale-[30%] [&:hover>*]:scale-110 [&:hover>*]:transition">
               <Image src={ProfileImage} alt="Godswill Ezihe" />
             </div>
-            <div className="grid gap-1 leading-tight tracking-tight">
-              <span className="text-4xl font-bold text-slate-100">
+            <div className="grid gap-1 leading-tight">
+              <h2 className="text-4xl font-bold tracking-tight text-slate-100">
                 Godswill Ezihe
-              </span>
-              <span className="text-lg font-semibold text-slate-500">
-                @gzkdev
+              </h2>
+              <span className="text-sm font-medium uppercase text-slate-500">
+                Software Engineer
               </span>
             </div>
             <div className="grid max-w-md gap-5 lg:items-start">
               <p>
-                I'm a software engineer from Nigeria, focused on building
-                performant and scalable applications that are infused with
-                excellent user experience
+                Hi there, I'm a software engineer from Nigeria. I focus on
+                building performant and scalable applications that are infused
+                with excellent user experience
               </p>
-              <a
-                href="#"
-                className="w-max rounded bg-teal-300 px-8 py-3 text-sm font-semibold uppercase tracking-tight text-teal-900 sm:w-max"
-              >
-                Get in touch
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="grid-item min-h-[50vh] lg:col-[3/5] lg:row-[1/4] lg:min-h-0">
-        <div className="wrapper">
-          <div className="grid h-full w-full">
-            <div className="sticky-header">About</div>
-          </div>
-        </div>
-      </div>
-      <div className="grid-item min-h-[50vh] lg:col-[3/5] lg:row-[4/7] lg:min-h-0">
-        <div className="wrapper">
-          <div className="grid h-full w-full">
-            <div className="sticky-header">Projects</div>
-          </div>
-        </div>
-      </div>
-      <div className="grid-item lg:col-[1/3] lg:row-[6/7]">
-        <div className="wrapper h-full">
-          <div className="flex h-full w-full flex-col">
-            <div className="flex flex-col gap-5 px-5 pb-16 lg:pb-5">
-              <ul className="flex w-full items-center justify-center gap-6 text-2xl lg:justify-start">
+              <ul className="flex max-w-max items-center justify-center gap-0.5 text-2xl lg:justify-start">
                 <li>
                   <a
                     href="http://#"
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="inline-flex aspect-square w-12  items-center justify-center rounded-full bg-slate-600/50 transition hover:text-teal-300"
+                    className="inline-flex aspect-square w-12 items-center justify-center bg-slate-600/50 transition hover:text-teal-300"
                     title="Twitter"
                   >
                     <IoLogoTwitter />
@@ -79,7 +52,7 @@ export default function Home() {
                     href="http://#"
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="inline-flex aspect-square w-12 items-center justify-center rounded-full bg-slate-600/50 transition hover:text-teal-300"
+                    className="inline-flex aspect-square w-12 items-center justify-center bg-slate-600/50 transition hover:text-teal-300"
                     title="Github"
                   >
                     <IoLogoGithub />
@@ -90,7 +63,7 @@ export default function Home() {
                     href="http://#"
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="inline-flex aspect-square w-12  items-center justify-center rounded-full bg-slate-600/50 transition hover:text-teal-300"
+                    className="inline-flex aspect-square w-12  items-center justify-center bg-slate-600/50 transition hover:text-teal-300"
                     title="LinkedIn"
                   >
                     <IoLogoLinkedin />
@@ -101,26 +74,38 @@ export default function Home() {
                     href="http://#"
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="inline-flex aspect-square w-12  items-center justify-center rounded-full bg-slate-600/50 transition hover:text-teal-300"
+                    className="inline-flex aspect-square w-12  items-center justify-center bg-slate-600/50 transition hover:text-teal-300"
                     title="Codepen"
                   >
                     <IoLogoCodepen />
                   </a>
                 </li>
               </ul>
-              <div className="text-center lg:text-start">
-                Designed & Built By{" "}
-                <a
-                  href="#"
-                  className="text-teal-300 underline decoration-dotted underline-offset-2"
-                >
-                  gzkdev
-                </a>
-              </div>
             </div>
           </div>
         </div>
       </div>
+      <div className="grid-item min-h-[70vh] lg:col-[3/5]">
+        <div className="wrapper">
+          <div className="grid h-full w-full">
+            <div className="sticky-header">About</div>
+          </div>
+        </div>
+      </div>
+      <div className="grid-item min-h-[50vh] lg:col-[3/5]">
+        <div className="wrapper">
+          <div className="grid h-full w-full">
+            <div className="sticky-header">Projects</div>
+          </div>
+        </div>
+      </div>
+      {/* <div className="grid-item min-h-[70vh] lg:col-[3/5]">
+        <div className="wrapper">
+          <div className="grid h-full w-full">
+            <div className="sticky-header">Articles</div>
+          </div>
+        </div>
+      </div> */}
     </main>
   );
 }
