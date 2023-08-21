9d2d4525f41ddb4fe3097d4c052bb850d655acb8
Ezihe Godswill <ezihegodswill01@gmail.com>
2023-08-21 11:39:16 +0100

    add markup for app home page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,7 +2,7 @@ import Loader from "@/components/Loader";
 
 export default function About() {
   return (
-    <main>
+    <main className="grid h-full gap-1 p-1">
       <Loader text="about" />
       <h1>About</h1>
     </main>
--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,23 @@ import Loader from "@/components/Loader";
 
 export default function Home() {
   return (
-    <main>
-      <Loader text="gzkdev" />
-      <h1>Home</h1>
+    <main className="grid min-h-full gap-1 p-1 lg:grid-cols-4 lg:grid-rows-4">
+      <Loader text="about" />
+      <div className="grid-item top-0 min-h-[80vh] lg:sticky lg:col-[1/3] lg:row-[1/6] lg:h-[99vh]">
+        <div className="wrapper"> 1</div>
+      </div>
+      <div className="grid-item min-h-[60vh] lg:col-[3/5]">
+        <div className="wrapper"> 2</div>
+      </div>
+      <div className="grid-item min-h-[50vh] lg:col-[3/5]">
+        <div className="wrapper"> 3</div>
+      </div>
+      <div className="grid-item min-h-[10vh] lg:col-[3/5]">
+        <div className="wrapper"> 4</div>
+      </div>
+      <div className="grid-item min-h-[10vh] lg:col-[3/5]">
+        <div className="wrapper"> 5</div>
+      </div>
     </main>
   );
 }
