7dca5bc1433822a36afe3b349efe32688cd83b2d
Ezihe Godswill <ezihegodswill01@gmail.com>
2023-08-21 18:21:31 +0100

    refactor home page markup

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,112 @@
 import Loader from "@/components/Loader";
+import {
+  IoLogoGithub,
+  IoLogoTwitter,
+  IoLogoLinkedin,
+  IoLogoCodepen,
+} from "react-icons/io";
 
 export default function Home() {
   return (
-    <main className="grid min-h-full gap-1 p-1 lg:grid-cols-4 lg:grid-rows-4">
-      <Loader text="about" />
-      <div className="grid-item top-0 min-h-[80vh] lg:sticky lg:col-[1/3] lg:row-[1/6] lg:h-[99vh]">
-        <div className="wrapper"> 1</div>
+    <main className="grid min-h-full gap-2 p-2 lg:max-h-screen lg:grid-cols-4 lg:grid-rows-6">
+      {/* [&>*]:outline */}
+      <Loader text="home" />
+      <div className="grid-item top-0 lg:sticky lg:col-[1/3] lg:row-[1/6]">
+        <div className="wrapper h-full w-full text-center lg:text-start">
+          <div className="flex w-full flex-col items-center px-5 py-16 lg:items-start">
+            <div className="relative aspect-square w-full max-w-[180px] overflow-hidden rounded border"></div>
+            <div className="my-5 flex flex-col gap-1">
+              <span className="text-4xl font-semibold leading-tight text-slate-100">
+                Godswill Ezihe
+              </span>
+              <span className="font-semibold text-slate-500">
+                Software Engineer
+              </span>
+            </div>
+            <div className="mb-10 flex max-w-md flex-col items-center gap-5 lg:items-start">
+              <p>
+                I'm a software engineer from Nigeria, focused on building
+                performant and scalable applications that are infused with
+                excellent user experience
+              </p>
+              <a
+                href="#"
+                className="sm rounded bg-teal-300 px-8 py-4 font-semibold text-teal-900 sm:w-max"
+              >
+                Get in touch
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="grid-item min-h-[60vh] lg:col-[3/5]">
-        <div className="wrapper"> 2</div>
+      <div className="grid-item min-h-[50vh] lg:col-[3/5] lg:row-[1/4] lg:min-h-0">
+        <div className="wrapper">
+          <div className="grid h-full w-full">
+            <div className="sticky-header">About</div>
+          </div>
+        </div>
       </div>
-      <div className="grid-item min-h-[50vh] lg:col-[3/5]">
-        <div className="wrapper"> 3</div>
+      <div className="grid-item min-h-[50vh] lg:col-[3/5] lg:row-[4/7] lg:min-h-0">
+        <div className="wrapper">
+          <div className="grid h-full w-full">
+            <div className="sticky-header">Projects</div>
+          </div>
+        </div>
       </div>
-      <div className="grid-item min-h-[10vh] lg:col-[3/5]">
-        <div className="wrapper"> 4</div>
-      </div>
-      <div className="grid-item min-h-[10vh] lg:col-[3/5]">
-        <div className="wrapper"> 5</div>
+      <div className="grid-item lg:col-[1/3] lg:row-[6/7]">
+        <div className="wrapper h-full">
+          <div className="flex h-full w-full flex-col">
+            <div className="sticky-header">Socials</div>
+            <div className="flex px-5 pb-5">
+              <ul className="flex items-center justify-center gap-6 text-3xl lg:justify-start">
+                <li>
+                  <a
+                    href="http://#"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="inline-flex items-center justify-center p-2 transition hover:bg-slate-600/50 hover:text-teal-300"
+                    title="Twitter"
+                  >
+                    <IoLogoTwitter />
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="http://#"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="inline-flex items-center justify-center p-2 transition hover:bg-slate-600/50 hover:text-teal-300"
+                    title="Github"
+                  >
+                    <IoLogoGithub />
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="http://#"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="inline-flex items-center justify-center p-2 transition hover:bg-slate-600/50 hover:text-teal-300"
+                    title="LinkedIn"
+                  >
+                    <IoLogoLinkedin />
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="http://#"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="inline-flex items-center justify-center p-2 transition hover:bg-slate-600/50 hover:text-teal-300"
+                    title="Codepen"
+                  >
+                    <IoLogoCodepen />
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   );
