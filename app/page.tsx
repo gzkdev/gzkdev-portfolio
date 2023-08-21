ad6de1198d4b307e355007580e17889bbf4e32c1
Ezihe Godswill <ezihegodswill01@gmail.com>
2023-08-21 20:14:37 +0100

    latest update

--- app/page.tsx
+++ app/page.tsx
@@ -16,16 +16,16 @@ export default function Home() {
       <div className="grid-item top-0 lg:sticky lg:col-[1/3] lg:row-[1/6]">
         <div className="wrapper h-full w-full  lg:text-start">
           <div className="sticky-header">Intro</div>
-          <div className="flex w-full flex-col gap-5 px-5 py-5 lg:items-start">
-            <div className="relative aspect-square w-full max-w-[144px] overflow-hidden rounded-full border-[4px] border-teal-500 grayscale-[50%] transition hover:grayscale-0">
+          <div className="flex w-full flex-col gap-5 px-5 py-5 pb-10 lg:items-start lg:pb-5">
+            <div className="relative aspect-square w-full max-w-[144px] overflow-hidden rounded border-[4px] border-teal-500 grayscale-[50%] transition hover:grayscale-[30%]">
               <Image src={ProfileImage} alt="Godswill Ezihe" />
             </div>
             <div className="grid gap-1 leading-tight tracking-tight">
-              <span className="text-5xl font-semibold text-slate-100">
+              <span className="text-4xl font-bold text-slate-100">
                 Godswill Ezihe
               </span>
-              <span className="font-medium uppercase text-slate-500">
-                Software Engineer
+              <span className="text-lg font-semibold text-slate-500">
+                @gzkdev
               </span>
             </div>
             <div className="grid max-w-md gap-5 lg:items-start">
@@ -61,9 +61,8 @@ export default function Home() {
       <div className="grid-item lg:col-[1/3] lg:row-[6/7]">
         <div className="wrapper h-full">
           <div className="flex h-full w-full flex-col">
-            <div className="sticky-header">Socials</div>
-            <div className="flex px-5 pb-16 lg:pb-5">
-              <ul className="flex w-full items-center justify-between gap-6 text-2xl lg:justify-start">
+            <div className="flex flex-col gap-5 px-5 pb-16 lg:pb-5">
+              <ul className="flex w-full items-center justify-center gap-6 text-2xl lg:justify-start">
                 <li>
                   <a
                     href="http://#"
@@ -109,6 +108,15 @@ export default function Home() {
                   </a>
                 </li>
               </ul>
+              <div className="text-center lg:text-start">
+                Designed & Built By{" "}
+                <a
+                  href="#"
+                  className="text-teal-300 underline decoration-dotted underline-offset-2"
+                >
+                  gzkdev
+                </a>
+              </div>
             </div>
           </div>
         </div>
